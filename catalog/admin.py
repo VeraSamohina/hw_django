dd84856cf7ea28303123ba69ef3b2372c91cca9b
Vera Samohina <samohinavera44@gmail.com>
2023-09-09 10:08:13 +0300

    add models Category and Product

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title')
