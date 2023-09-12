45f19d83bf3e5a48a388e6e828dfb97c2c07bca5
Vera Samohina <samohinavera44@gmail.com>
2023-09-12 10:59:50 +0300

    add page for each product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('<int:pk>/product/', product, name='product')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 def home(request):
@@ -19,3 +19,16 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'You have new feedback from {name}({phone}): {message}')
     return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'title': product_item.title,
+        'overview': product_item.description,
+        'price': product_item.price,
+        'category': product_item.category,
+        'date_create': product_item.date_create,
+        'date_change': product_item.date_last_change
+    }
+    return render(request, 'catalog/product.html', context)
