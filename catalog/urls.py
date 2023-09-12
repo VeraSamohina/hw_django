1b70b02fe88d419f6c119221e9390b847ff1206b
Vera Samohina <samohinavera44@gmail.com>
2023-09-12 09:40:48 +0300

    change urls, add context in views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-
 from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/homepage.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Магазин хороших товаров',
+        'description': 'Предлагаем различные товары по привлекательным ценам'}
+    return render(request, 'catalog/homepage.html', context)
 
 
 def contacts(request):
+    context = {'title': 'Наши контакты'}
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new feedback from {name}({phone}): {message}')
-
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
