23fdc178e049a4c591f9fe99effa00855ee4366e
Vera Samohina <samohinavera44@gmail.com>
2023-09-15 20:34:02 +0300

    change model add migrations

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=250, verbose_name='статья')
-    slug = models.CharField(verbose_name='slug')
+    slug = models.CharField(max_length=150, blank=True, null=True, verbose_name='slug')
     body = models.TextField(verbose_name='содержание')
-    preview = models.ImageField(upload_to='previews/', verbose_name='превью')
+    preview = models.ImageField(upload_to='previews/', blank=True, null=True, verbose_name='превью')
     date_create = models.DateField(auto_now_add=True, verbose_name='дата создания')
     is_published = models.BooleanField(default=True, verbose_name='не опубликовано')
     count_view = models.IntegerField(default=0, verbose_name='количество просмотров')
