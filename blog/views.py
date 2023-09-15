01f69cf9cd8dabf30a7e6ceb6f3605c58bc23335
Vera Samohina <samohinavera44@gmail.com>
2023-09-15 20:52:12 +0300

    add slug

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView
+from pytils.translit import slugify
 
 from blog.models import Article
 
@@ -9,6 +10,15 @@ class ArticleCreateView(CreateView):
     model = Article
     fields = ('title', 'body', 'preview')
     success_url = reverse_lazy('blog:blog')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            new_article = form.save()
+            new_article.slug = slugify(new_article.title)
+            new_article.save()
+
+        return super().form_valid(form)
+
 
 
 class ArticleListView(ListView):
