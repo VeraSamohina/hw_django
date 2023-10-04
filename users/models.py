e06e226297cc1be50b39ec3efc80f52b63a10db9
Vera Samohina <samohinavera44@gmail.com>
2023-10-04 10:53:13 +0300

    add verifacate and generate new password

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    is_verified = models.BooleanField(default=False, verbose_name='почта верифицирована')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
+import random
+
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView, UpdateView
 
+from config import settings
 from users.models import User
-from users.templates.users.forms import UserForm
+from users.forms import UserRegisterForm, UserForm
 
 
 class LoginView(BaseLoginView):
@@ -16,7 +21,58 @@ class LogoutView(BaseLogoutView):
 
 
 class RegisterView(CreateView):
+    extra_context = {'title': 'Регистрация нового пользователя'}
     model = User
-    form_class = UserForm
+    form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
     template_name = 'users/register.html'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        text_message = ("Вы успешно зарегистрированы на сайте Магазина Хороших товаров. "
+                        "Для подтверждения электронной почты перейдите пожалуйста по ссылке")
+        id = self.object.id
+        url = f"{self.request.build_absolute_uri('verification')}/{id}"
+        send_mail(
+            subject='Вы зарегистрированы',
+            message=f'{text_message} {url}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[self.object.email],
+            fail_silently=False
+        )
+        return super().form_valid(form)
+
+
+class VerificationView(TemplateView):
+    template_name = 'users/verification.html'
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+        user = User.objects.get(pk=pk)
+        user.is_verified = True
+        user.save()
+        return super(VerificationView, self).get(request, *args, **kwargs)
+
+
+class UserUpdateView(UpdateView):
+    extra_context = {'title': 'Редактирование профиля'}
+    model = User
+    success_url = reverse_lazy('users:profile')
+    form_class = UserForm
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+def generate_password(request):
+    new_password = ''.join([str(random.randint(0,9)) for _ in range(10)])
+    send_mail(
+        subject='Вы успешно сменили пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+
+    return redirect(reverse('catalog:home'))
